79182e5ab5da9f7c4ce385cc4e8c48b2bbb55004
BonoLoveDitto <tsaibrian0730@gmail.com>
2023-09-18 20:03:54 +0800

    message

---
+++ Assets/Scripts/RemoveCard.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleCardDrawAndSpread_RemoveCard{
+    public class RemoveCard : MonoBehaviour
+    {
+        public int id;
+        public int turn;
+    }
+}
